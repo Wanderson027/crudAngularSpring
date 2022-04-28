cb912a182a9f7ff25afbf30ecef3a56f9e7db525
Wandeson027 <w.rafael.s@hotmail.com>
2022-04-28 17:47:23 -0300

    adicionado tratamento de error ao carrear json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -14,7 +14,12 @@ export class CoursesComponent implements OnInit {
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(error => {
+        console.log(error)
+        return of([])
+      })
+    )
   }
 
   ngOnInit(): void {}
